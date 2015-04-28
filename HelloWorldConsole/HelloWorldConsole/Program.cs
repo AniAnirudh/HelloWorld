1747c446494859ab90393081cbfd6649331f7a76
ntdev\anirudha <anirudha@microsoft.com>
2015-04-28 14:55:22 -0700

    firstcom

---
+++ HelloWorldConsole/HelloWorldConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HelloWorld");
+            Console.Write("\n\tPress g: \n");
+            String reply = Console.ReadLine();
+            Console.Write("\t\t"+reply);
+            Console.ReadKey();
+        }
+    }
+}
