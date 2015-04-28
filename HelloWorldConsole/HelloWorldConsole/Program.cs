f7788d85d99bda62ef444a3464099beb5906dd52
asydAum <ani.asydaum@gmail.com>
2015-04-28 16:05:31 -0700

    Update Program.cs

--- HelloWorldConsole/HelloWorldConsole/Program.cs
+++ HelloWorldConsole/HelloWorldConsole/Program.cs
@@ -2,6 +2,7 @@
 //File Header//
 //Name: Program.cs
 //Purpose: Main();
+// implements basic Question/Response system using console I/O.
 
 // list of libraries - add only what is needed. and mention why.
 ﻿using System;
